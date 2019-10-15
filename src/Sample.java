6dd0a72ac5ec51b275a3072e57b3f7b14a88df1d
Kumar, Abhay <akumar@rxsense.com>
2019-10-15 10:38:01 -0400

    Commiting

--- src/Sample.java
+++ src/Sample.java
@@ -2,4 +2,5 @@ public class Sample {    public static void main(String[] args) {        int tes
 } else if (testscore >= 80) {            grade = 'B';        } else if (testscore >= 70) {            grade = 'C';        } else if (testscore >= 60) {
     grade = 'D';        } else {            grade = 'F';        }        System.out.println("Grade = " + grade);
 }
+
 }
